472b73ccd3cc15d223eec596ec609e127be38124
YashMishra0101 <yashrkmfedu6927@gmail.com>
2024-06-09 23:18:07 +0530

    Update .gitignore and remove ignored files from tracking

--- src/component/index.js
+++
@@ -1,8 +0,0 @@
-export {default as About } from "../pages/About.jsx";
-export {default as EmergencyContacts } from "../pages/EmergencyContacts.jsx";
-export {default as Home } from "../pages/Home.jsx";
-export {default as Login } from "../pages/Login.jsx";
-export {default as ReachOut } from "../pages/ReachOut.jsx";
-export {default as SignUp } from "../pages/SignUp.jsx";
-export {default as SupportResources } from "../pages/SupportResources.jsx";
-export {default as  VideosHelp} from "../pages/VideosHelp.jsx";
